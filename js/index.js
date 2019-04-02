2f76ab33056e3c4eb8e80cbc5ddeb0bc53dad61b
Mykhailo Dulyba <mykhailodulyba@gmail.com>
2019-04-03 00:08:49 +0300

    about-start

--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
+
+// $("#home").on('click',  () => {
+//     $("#about-card").addClass('action'); 
+//     setTimeout(()=>{
+//         $("#content").removeClass('action'); 
+//     }, 1500);
+//     setTimeout(()=>{
+//         $("#home-card").css({'left': '0%'}); 
+//         $("#about-card").css({'left': '-100%'});
+//     }, 3000);    
+// });
+
+
+
 $("#about").on('click',  () => {
-    $("#about-card").removeClass('off-card');  
-    $("#about-card").addClass('on-card');
+    // $("#content").addClass('action'); 
+    // setTimeout(()=>{
+    //     $("#content").removeClass('action'); 
+    // }, 1500);
+    // $("#home-card").css({'left': '-100%'}); 
+    // $("#about-card").css({'left': '0%'});  
+    $("#contact-card").css({'left': '0%'}); 
+    $("#home-card").addClass('head_top '); 
 });
 
-$("#home").on('click',  () => {
-    $("#about-card").addClass('off-card');    
-});
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
