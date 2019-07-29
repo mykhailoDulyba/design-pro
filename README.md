58c7005ffa85749406dc588b16827436fb90d943
Mykhailo Dulyba <mykhailodulyba@gmail.com>
2019-07-29 23:08:41 +0300

    proceed working Fix mobile menu and Home page adapt

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # design-pro
+
+// let myVideo = document.getElementById("video"); 
+// document.getElementById("video").addEventListener('click', (event) => {
+//   if(myVideo.paused){
+//     setTimeout(() => {   
+//       myVideo.play();
+//       myVideo.controls = true;    
+//   }, 380); 
+
+//   myVideo.classList.add("goVideo");
+//   const goo= $("#content")
+//   goo.addClass('change');
+//   }else {
+//     $("#video").removeClass("goVideo");
+//     const goo= $("#content")
+//     $("#video").addClass("then");
+//     $("#video").removeClass("goVideo");
+//     setTimeout(() => {   
+//       $("#video").removeClass("then");
+//   }, 420); 
+//   goo.removeClass('change');  
+    
+//   }
+//   myVideo.addEventListener('ended', function() {
+//     myVideo.load();     
+//   });
+
+//   console.log(myVideo);
+// });
--- js/index.js
+++ js/index.js
@@ -1,194 +1,142 @@
-// window.addEventListener("load", showPage);
+$(document).ready(init());
 
-// function showPage() {
-//   $('.layer').removeClass('data'); 
-// }
+function init() {
+  // Preloader
+  $('.layer').removeClass('data');
 
-$(document).ready(function() {
-  $('.layer').removeClass('data'); 
-
-
-
-
-
-$(".work-bt").click(function(){
-  var value = $(this).attr("data-filter");
-  if (value == "all")
-  {
-    $(".filter").show("1000");
-  }
-  else
-  {
-    $(".filter").not("."+value).hide("1000");
-    $(".filter").filter("."+value).show("1000");
-  }
-});
-
-
-function myMap() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-
-// let myVideo = document.getElementById("video"); 
-// document.getElementById("video").addEventListener('click', (event) => {
-//   if(myVideo.paused){
-//     setTimeout(() => {   
-//       myVideo.play();
-//       myVideo.controls = true;    
-//   }, 380); 
-
-//   myVideo.classList.add("goVideo");
-//   const goo= $("#content")
-//   goo.addClass('change');
-//   }else {
-//     $("#video").removeClass("goVideo");
-//     const goo= $("#content")
-//     $("#video").addClass("then");
-//     $("#video").removeClass("goVideo");
-//     setTimeout(() => {   
-//       $("#video").removeClass("then");
-//   }, 420); 
-//   goo.removeClass('change');  
-    
-//   }
-//   myVideo.addEventListener('ended', function() {
-//     myVideo.load();     
-//   });
-
-//   console.log(myVideo);
-// });
-
-
-$('.count').each(function() {
-  var $this = this;
-  let countTo = $this.dataset.count;
-  let localStart = 0;
-  setInterval(function() {
-    if (localStart < countTo) {
-      localStart++;
-      $this.innerHTML = localStart;
+  // Gallery filter
+  $(".work-bt").click(function(){
+    var value = $(this).attr("data-filter");
+    if (value == "all")
+    {
+      $(".filter").show("1000");
     }
-  }, 40);
-  console.log(countTo);
-  
-});
+    else
+    {
+      $(".filter").not("."+value).hide("1000");
+      $(".filter").filter("."+value).show("1000");
+    }
+  });
 
-
-
-
-(function() {
-    let navLinks = $('nav ul li');
-    const sections = $('.card-section');   
-    navLinks.click(function(){ 
-               
-         
-      // let ad = $('.up1')
-      // ad.css({"animation": "close 1s cubic-bezier(0.740, 0.150, 0.835, 0.345) forwards"});
-      // let dod = $('.up2')
-      // dod.css({"animation": "close 1.3s cubic-bezier(0.740, 0.150, 0.835, 0.345) forwards"}); 
-    
-   
-          let self = $(this);
-          let target = self.children().attr("data-page");
-          let targetClass = '.' + target;
-          let targetSection = $(`${targetClass}-card`);
-          let menuType = self.parent().parent().hasClass('main-menu');
-
-          navLinks.each((el, val) => {
-            const currentNav = val.className;
-            if(currentNav.includes('active-nav')){
-              val.classList.remove('active-nav');
-              
-            }            
-          });  
-
-          setTimeout(()=> {
-            sections.each((el, val) => {
-              var currentSection = val.className;
-              if(currentSection.includes('active-section')){
-                val.classList.remove('active-section');
-              }            
-            });
-            if(target === 'about'){
-              $('#main-card').css("background-image", "url(image/Lines.png), url(image/About_background.jpg)")
-            } else if(target === 'home'){
-              $('#main-card').css("background-image", "url(image/Lines.png), url(image/Background.jpg)")
-            }else {
-              $('#main-card').css("background-image", "none");
-            } 
-            targetSection.addClass('active-section'); 
-          }, 600);
-
-          $(targetClass).addClass('active-nav');
-          if(!menuType) {
-
-          }
-                 
- 
-// setTimeout(()=> {
-//   let ad = $('.up1')
-//   ad.css({"animation": "opend 1.6s ease-in-out forwards"});
-// }, 1300);
-
-// setTimeout(()=> {
-//   let dodo = $('.up2')
-//   dodo.css({"animation": "opend 1s ease-in-out forwards"});
-// }, 1300);
-         
-    });
-})();
-
-
-$('.social-btn').click(function(el) {
-
-  // $('.social-btn svg')[0].dataset.icon = 'angle-left'
-  console.log( $('.social-btn svg'));
-  $('.home-social-icon').toggleClass('active-social')
-  var btnIcon = $('.social-btn svg')[0].dataset.icon
-  if(btnIcon === 'angle-left'){
-    $('.social-btn svg')[0].dataset.icon = 'angle-right';
-  } else {
-    $('.social-btn svg')[0].dataset.icon = 'angle-left';
+  // Google map
+  function myMap() {
+    var mapProp = {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
   }
-  console.log( $('.social-btn svg'));
-  
-});    
+
+  //About counter
+  $('.count').each(function() {
+    var $this = this;
+    let countTo = $this.dataset.count;
+    let localStart = 0;
+    setInterval(function() {
+      if (localStart < countTo) {
+        localStart++;
+        $this.innerHTML = localStart;
+      }
+    }, 40);  
+  });
+
+
+
+
+  let navLinks = $('nav ul li');
+  const sections = $('.card-section'); 
+
+  navLinks.click(function(){
+    let self = $(this);
+    let target = self.children().attr("data-page");
+    let targetClass = '.' + target;
+    let targetSection = $(`${targetClass}-card`);
+    let menuType = self.parent().parent().hasClass('main-menu');
+
+    navLinks.each((el, val) => {
+      const currentNav = val.className;
+      if(currentNav.includes('active-nav')){
+        val.classList.remove('active-nav');
+        
+      }            
+    });
+
+    sections.each((el, val) => {
+      var currentSection = val.className;
+      if(currentSection.includes('active-section')){
+        val.classList.remove('active-section');
+      }            
+    });
+
+    if(target === 'about'){
+      $('#main-card').css("background-image", "url(image/Lines.png), url(image/About_background.jpg)")
+    } else if(target === 'home'){
+      $('#main-card').css("background-image", "url(image/Lines.png), url(image/Background.jpg)")
+    }else {
+      $('#main-card').css("background-image", "none");
+    } 
+    targetSection.addClass('active-section');
+
+    $(targetClass).addClass('active-nav');
+
+    var menuItems =  getMenuItems();
+    menuAnimationOff(menuItems);
+    menu.removeClass('act-mobile');
+   
+  });
+
+  // Social button
+
+  $('.social-btn').click(function(el) {
+    $('.home-social-icon').toggleClass('active-social')
+    var btnIcon = $('.social-btn svg')[0].dataset.icon
+    if(btnIcon === 'angle-left'){
+      $('.social-btn svg')[0].dataset.icon = 'angle-right';
+    } else {
+      $('.social-btn svg')[0].dataset.icon = 'angle-left';
+    }    
+  });    
 
 // Animation burger menu 
 
-var touch = $('#burger-menu');
-var menu = $('.mobile');
+  var touch = $('#burger-menu');
+  var menu = $('.mobile');
 
+  $(touch).click(function() {
+    menu.toggleClass('act-mobile');
+    menuAnimation();
+  });
 
-$(touch).click(function(e) {
-  e.preventDefault();
-  menu.toggleClass('act-mobile');
-  var arr = [];  
-    $('.mobile-menu ul li').each((el, val) => {     
-      arr.push(val);                
-    });  
-
+  function menuAnimation() {
+    var menuItems =  getMenuItems();
     if(menu.hasClass('act-mobile')) {
-      arr.forEach((val, index) => {
-        $(val).css({"animation": `dreg-out ${0.6 + (0.1*index)}s cubic-bezier(1.000, 0.015, 0.325, 0.985) forwards`});
-      });
+      menuAnimationOn(menuItems); 
+    } else {
+      menuAnimationOff(menuItems);
+    }
+  };
 
-     } else {
-      arr.reverse();
-      arr.forEach((val, index) => {
-        $(val).css({"animation": `dreg-in ${0.6 - (0.1*index)}s cubic-bezier(1.000, 0.015, 0.325, 0.985) forwards`});
-      });
-     }
+  function getMenuItems() {    
+    var menuItems = [];  
+    $('.mobile-menu ul li').each((el, val) => {     
+      menuItems.push(val);                
+    });
+    return menuItems;
+  }
+
+  function menuAnimationOn(menuItems) {
+    menuItems.forEach((val, index) => {
+      $(val).css({"animation": `dreg-out ${0.6 + (0.1*index)}s cubic-bezier(1.000, 0.015, 0.325, 0.985) forwards`});
+    });
+  }
+
+  function menuAnimationOff(menuItems){
+    menuItems.reverse();
+    menuItems.forEach((val, index) => {
+      $(val).css({"animation": `dreg-in ${0.6 - (0.1*index)}s cubic-bezier(1.000, 0.015, 0.325, 0.985) forwards`});
+    });
+  }
 
 
- 
-
-});
-
-
-});
+};
 
